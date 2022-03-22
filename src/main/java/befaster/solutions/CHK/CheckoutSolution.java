aefb52874abaebaba479ce0a1f742b1bc478bc3d
sbx_user1051 <sbx_user1051@169.254.240.9>
2022-03-22 21:03:52 +0000

    Tue Mar 22 21:03:52 UTC 2022

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -4,32 +4,36 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        int noOfA = 0;
-        int noOfB = 0;
-        int noOfC = 0;
-        int noOfD = 0;
+        if(skus != null) {
 
-        for(int i = 0; i < skus.length(); i++){
-            if(skus.charAt(i) == 'A')
-                noOfA++;
-            else if(skus.charAt(i) == 'B')
-                noOfB++;
-            else if(skus.charAt(i) == 'C')
-                noOfC++;
-            else if(skus.charAt(i) == 'D')
-                noOfD++;
+            int noOfA = 0;
+            int noOfB = 0;
+            int noOfC = 0;
+            int noOfD = 0;
+
+
+            for (int i = 0; i < skus.length(); i++) {
+                if (skus.charAt(i) == 'A')
+                    noOfA++;
+                else if (skus.charAt(i) == 'B')
+                    noOfB++;
+                else if (skus.charAt(i) == 'C')
+                    noOfC++;
+                else if (skus.charAt(i) == 'D')
+                    noOfD++;
+            }
+        return calculatePriceA(noOfA) + calculatePriceB(noOfB) + 20 * noOfC + 15 * noOfD;
         }
-
-
+    return -1;
     }
 
     private int calculatePriceA(int noOfA){
-        int priceOfA = 0;
-
-            priceOfA = 130 * (noOfA / 3) + 50 * (noOfA % 3);
-        
+        return 130 * (noOfA / 3) + 50 * (noOfA % 3);
     }
+
+    private int calculatePriceB(int noOfB){
+        return 45 * (noOfB / 2) + 30 * (noOfB % 2);
+    }
+
+
 }
-
-
-
